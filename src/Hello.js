685a399afa9e9984b8c96e0c59eeec7763f3cc2e
Muhammad Faizan <57411742+MFaizan103@users.noreply.github.com>
2020-06-05 13:11:02 +0500

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,7 @@ import './Hello.css';
 class Hello extends Component {
     render(){
         return( 
-        <div className="f5 tc white grow courier bg-dark-pink">
+        <div className="f1 tc yellow grow courier bg-dark-pink">
             <h1>Hello World!</h1>
         <p>{this.props.greet}</p>
         </div>
